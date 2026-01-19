4228b7441964ba26b48b016d95d204d7687ad1e5
Nada Al-Mutairi <nada.ghannam19@gmail.com>
2026-01-19 14:51:41 +0300

    Set up database engine and create metadata
    Initialize the database engine and create tables.

--- part3/hbnb/run.py
+++ part3/hbnb/run.py
@@ -1,7 +1,12 @@
 from app import create_app
 from config import DevelopmentConfig
+from app.db.database import get_engine
+from app.models.base_model import Base
 
 app = create_app(DevelopmentConfig)
+
+engine = get_engine("development")
+Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
     app.run(debug=True)

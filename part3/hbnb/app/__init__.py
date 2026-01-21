ad8e32b180c8db46c217189318de78fc15a0b325
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-21 12:10:03 +0300

    Uncomment amenities_ns import in __init__.py

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.auth import api as auth_ns
-#from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 

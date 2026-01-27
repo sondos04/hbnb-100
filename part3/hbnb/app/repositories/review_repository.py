6af2faab1ee7f5f932fbdb714e1ce51ad0987006
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-27 17:18:06 +0300

    Fix constructor method in ReviewRepository

--- part3/hbnb/app/repositories/review_repository.py
+++ part3/hbnb/app/repositories/review_repository.py
@@ -1,10 +1,15 @@
+from app.extensions import db
 from app.repositories.sqlalchemy_repository import SQLAlchemyRepository
 from app.models.review import Review
 
 
 class ReviewRepository(SQLAlchemyRepository):
-    def init(self):
-        super().init(Review)
+    def __init__(self):
+        super().__init__(Review)
 
     def get_by_user_and_place(self, user_id, place_id):
-        return Review.query.filter_by(user_id=user_id, place_id=place_id).first()
+        return (
+            self.model.query
+            .filter_by(user_id=user_id, place_id=place_id)
+            .first()
+        )

f124f56477217237b6231d374749a871912b7227
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-27 17:17:40 +0300

    Fix constructor method in PlaceRepository

--- part3/hbnb/app/repositories/place_repository.py
+++ part3/hbnb/app/repositories/place_repository.py
@@ -1,10 +1,11 @@
+from app.extensions import db
 from app.repositories.sqlalchemy_repository import SQLAlchemyRepository
 from app.models.place import Place
 
 
 class PlaceRepository(SQLAlchemyRepository):
-    def init(self):
-        super().init(Place)
+    def __init__(self):
+        super().__init__(Place)
 
     def get_by_owner(self, user_id):
-        return Place.query.filter_by(owner_id=user_id).all()
+        return self.model.query.filter_by(owner_id=user_id).all()

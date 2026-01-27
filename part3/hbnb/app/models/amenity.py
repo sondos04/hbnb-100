50d3926fdc38f897757b63d2e3493f5577a3d15a
Shaden Alalwani <viuxwl7@gmail.com>
2026-01-27 16:43:05 +0300

    Remove Amenity class from amenity.py

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,16 +1,3 @@
 from sqlalchemy import Column, String
 from app.models.base_model import Base, BaseModel
 
-
-class Amenity(Base, BaseModel):
-    __tablename__ = "amenities"
-
-    name = Column(String(50), nullable=False, unique=True)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "created_at": self.created_at.isoformat() if self.created_at else None,
-            "updated_at": self.updated_at.isoformat() if self.updated_at else None,
-        }

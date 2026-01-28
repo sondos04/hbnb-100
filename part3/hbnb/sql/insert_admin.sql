71aaa5fa92d67f2b121c42e5bbcfebb531fc420f
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-01-29 01:59:43 +0300

    Update insert_admin.sql

--- part3/hbnb/sql/insert_admin.sql
+++ part3/hbnb/sql/insert_admin.sql
@@ -1,13 +1,14 @@
+DELETE FROM users WHERE email = 'admin@hbnb.io';
+
 INSERT INTO users (
     id, email, password, first_name, last_name, is_admin, created_at, updated_at
 ) VALUES (
     '00000000-0000-0000-0000-000000000001',
     'admin@hbnb.io',
-    'admin',
+    '$2b$12$oJts4jSRERjf4K5GlI/9qeac2YRhRkAaz6/TmgjcqAY2rwBwsH2k6',
     'Admin',
     'User',
     1,
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP
 );
-
